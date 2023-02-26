8369d22a01f65e3dda8904cb388db1b7d1d09aa4
bakuzan <imannoyed4days@hotmail.co.uk>
2023-02-26 19:04:26 +0000

    Final chapters of the guide

--- apps/kv_server/lib/kv_server/command.ex
+++ apps/kv_server/lib/kv_server/command.ex
@@ -1,33 +1,33 @@
 defmodule KVServer.Command do
   @doc ~S"""
   Parses the given `line` into a command.
-  
+
   ## Examples
-  
+
       iex> KVServer.Command.parse "CREATE shopping\r\n"
       {:ok, {:create, "shopping"}}
-  
+
       iex> KVServer.Command.parse "CREATE  shopping  \r\n"
       {:ok, {:create, "shopping"}}
-  
+
       iex> KVServer.Command.parse "PUT shopping milk 1\r\n"
       {:ok, {:put, "shopping", "milk", "1"}}
-  
+
       iex> KVServer.Command.parse "GET shopping milk\r\n"
       {:ok, {:get, "shopping", "milk"}}
-  
+
       iex> KVServer.Command.parse "DELETE shopping eggs\r\n"
       {:ok, {:delete, "shopping", "eggs"}}
-  
+
   Unknown commands or commands with the wrong number of
   arguments return an error:
-  
+
       iex> KVServer.Command.parse "UNKNOWN shopping eggs\r\n"
       {:error, :unknown_command}
-  
+
       iex> KVServer.Command.parse "GET shopping\r\n"
       {:error, :unknown_command}
-  
+
   """
   def parse(line) do
     case String.split(line) do
@@ -45,8 +45,10 @@ defmodule KVServer.Command do
   def run(command)
 
   def run({:create, bucket}) do
-    KV.Registry.create(KV.Registry, bucket)
-    {:ok, "OK\r\n"}
+    case KV.Router.route(bucket, KV.Registry, :create, [KV.Registry, bucket]) do
+      pid when is_pid(pid) -> {:ok, "OK\r\n"}
+      _ -> {:error, "FAILED TO CREATE BUCKET"}
+    end
   end
 
   def run({:get, bucket, key}) do
@@ -71,7 +73,7 @@ defmodule KVServer.Command do
   end
 
   defp lookup(bucket, callback) do
-    case KV.Registry.lookup(KV.Registry, bucket) do
+    case KV.Router.route(bucket, KV.Registry, :lookup, [KV.Registry, bucket]) do
       {:ok, pid} -> callback.(pid)
       :error -> {:error, :not_found}
     end
--- apps/kv_server/test/kv_server_test.exs
+++ apps/kv_server/test/kv_server_test.exs
@@ -1,7 +1,7 @@
 defmodule KVServerTest do
   use ExUnit.Case
   @moduletag :capture_log
-  
+
   setup do
     Application.stop(:kv)
     :ok = Application.start(:kv)
@@ -15,23 +15,23 @@ defmodule KVServerTest do
 
   test "server interaction", %{socket: socket} do
     assert send_and_recv(socket, "UNKNOWN shopping\r\n") ==
-           "UNKNOWN COMMAND\r\n"
+             "UNKNOWN COMMAND\r\n"
 
     assert send_and_recv(socket, "GET shopping eggs\r\n") ==
-           "NOT FOUND\r\n"
+             "NOT FOUND\r\n"
 
     assert send_and_recv(socket, "CREATE shopping\r\n") ==
-           "OK\r\n"
+             "OK\r\n"
 
     assert send_and_recv(socket, "PUT shopping eggs 3\r\n") ==
-           "OK\r\n"
+             "OK\r\n"
 
     # GET returns two lines
     assert send_and_recv(socket, "GET shopping eggs\r\n") == "3\r\n"
     assert send_and_recv(socket, "") == "OK\r\n"
 
     assert send_and_recv(socket, "DELETE shopping eggs\r\n") ==
-           "OK\r\n"
+             "OK\r\n"
 
     # GET returns two lines
     assert send_and_recv(socket, "GET shopping eggs\r\n") == "\r\n"
@@ -40,7 +40,7 @@ defmodule KVServerTest do
 
   defp send_and_recv(socket, command) do
     :ok = :gen_tcp.send(socket, command)
-    {:ok, data} = :gen_tcp.recv(socket, 0, 1000)
+    {:ok, data} = :gen_tcp.recv(socket, 0, 2000)
     data
   end
 end
